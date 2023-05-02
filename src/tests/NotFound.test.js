acaa4453d9d0a2a6cc6e9c560e78b980177bc89c
Letícia Chaves <leticiaschaves7@gmail.com>
2023-05-02 17:18:25 -0300

    add req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Test NotFound component', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+  it('Should have a heading with Page requested not found text', () => {
+    const nofFound = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(nofFound).toBeInTheDocument();
+  });
+  it('Should render the image', () => {
+    const pikachuCrying = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(pikachuCrying).toBeInTheDocument();
+
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(pikachuCrying.getAttribute('src')).toBe(src);
+  });
+});
