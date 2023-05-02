0fabc2f341a31e0a9b6cf005ad5837f8191fda36
Letícia Chaves <leticiaschaves7@gmail.com>
2023-05-02 16:59:01 -0300

    add req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,3 +1,25 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import About from '../pages/About';
+import renderWithRouter from '../renderWithRouter';
 
-// pr do projeto
+describe('Test About component', () => {
+  beforeEach(() => {
+    renderWithRouter(<About />);
+  });
+  it('Should have a heading with About Pokemon text', () => {
+    const title = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(title).toBeInTheDocument();
+  });
+  it('Should have two paragraphs about Pokédex', () => {
+    const title = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(title).toBeInTheDocument();
+  });
+  it('Should render the image', () => {
+    const pokedexImg = screen.getByRole('img', { name: /pokédex/i });
+    expect(pokedexImg).toBeInTheDocument();
+
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(pokedexImg.getAttribute('src')).toBe(src);
+  });
+});
